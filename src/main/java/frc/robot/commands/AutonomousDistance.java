525b46611488962cdaf84fd00ae36b9829d4d061
Hawkbot Mentor <hawkbotmentor@stcs.org>
2023-03-08 20:19:26 -0500

    BB:One more thing to do, the darn return true

--- src/main/java/frc/robot/commands/AutonomousDistance.java
+++ src/main/java/frc/robot/commands/AutonomousDistance.java
@@ -4,14 +4,19 @@
 
 package frc.robot.commands;
 
+import javax.swing.text.Position;
+
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.SelectCommand;
 import frc.robot.subsystems.DriveSubsystem;
 import pabeles.concurrency.ConcurrencyOps.Reset;
 
 public class AutonomousDistance extends CommandBase {
+  private static final String Position = null;
   private final DriveSubsystem s_robotDrive;
   private final double distance;
   private final double speed;
+  private Object RobotPosition;
 
 
   /** Creates a new AutonomousDistance. */
@@ -28,26 +33,30 @@ public class AutonomousDistance extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    // TODO: resetEncoders
+    s_robotDrive.resetEncoders();
+
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    // TODO: drive
+    
+    drive();
   }
 
   // Called once the command ends or is interrupted.
+  private void drive() {
+  }
+
   @Override
   public void end(boolean interrupted) {
-    // TODO: stopMotors
+    s_robotDrive.stopMotors();
   }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
     // TODO return true when robot reaches position. Hint: getRobotPosition
-    
-    return true;
+    return true 
   }
 }
